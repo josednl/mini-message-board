0e688068542d05c79397a64cca6f0928a5d1c5d8
JoseD971 <157433709+JoseD971@users.noreply.github.com>
2025-08-28 13:29:45 -0500

    feat: add delete button to each message

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,34 +1,45 @@
 const { getAllMessages, addMessage, getMessageById } = require('../models/messageModel');
 
 function showAllMessages(req, res) {
-    const messages = getAllMessages();
-    res.render('index', { messages });
+	const messages = getAllMessages();
+	res.render('index', { messages });
 }
 
 function createMessage(req, res) {
-    const { user, text } = req.body;
+	const { user, text } = req.body;
 
-    if(!user || !text) {
-        return res.status(404).send('User and text are required');
-    }
+	if (!user || !text) {
+		return res.status(404).send('User and text are required');
+	}
 
-    addMessage(user, text);
-    res.redirect('/');
+	addMessage(user, text);
+	res.redirect('/');
 }
 
 function showMessageDetail(req, res) {
-    const messageId = req.params.id;
-    const message = getMessageById(messageId);
+	const messageId = req.params.id;
+	const message = getMessageById(messageId);
 
-    if(!message) {
-        return res.status(404).render('404');
-    }
+	if (!message) {
+		return res.status(404).render('404');
+	}
 
-    res.render('message-detail', { message });
+	res.render('message-detail', { message });
+}
+
+function deleteMessageById(id) {
+	const messages = getAllMessages();
+	const index = messages.findIndex((m) => m.id === id);
+	if (index !== -1) {
+		messages.splice(index, 1);
+		return true;
+	}
+	return false;
 }
 
 module.exports = {
-    showAllMessages,
-    createMessage,
-    showMessageDetail
-}
+	showAllMessages,
+	createMessage,
+	showMessageDetail,
+    deleteMessageById
+};
--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const path = require('path');
 const router = Router();
-const { showAllMessages, createMessage, showMessageDetail } = require('../controllers/messageController');
+const { showAllMessages, createMessage, showMessageDetail, deleteMessageById } = require('../controllers/messageController');
 
 router.get(['/', '/index'], showAllMessages);
 
@@ -10,5 +10,13 @@ router.get('/new', (req, res) => {
 });
 router.get('/messages/:id', showMessageDetail);
 router.post('/new', createMessage);
+router.post('/delete/:id', (req, res) => {
+    const { id } = req.params;
+    const deleted = deleteMessageById(id);
+    if (!deleted) {
+        return res.status(404).render('404');
+    }
+    res.redirect('/');
+});
 
 module.exports = router;
